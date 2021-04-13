4da1c8e9b42602734b52b79175a0d33b229b8ee9
Juan Galarce <j6alarce@gmail.com>
2021-04-13 11:15:39 -0400

    Diseño inicial y pantalla de Calculadora

--- App.tsx
+++ App.tsx
@@ -1,14 +1,15 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from 'react';
+import {SafeAreaView, StatusBar} from 'react-native';
+import {CalculadoraScreen} from './src/screens/CalculadoraScreen';
+import {styles} from './src/theme/appTheme';
 
 const App = () => {
   return (
-    <View>
-      <Text>
-        Hola Mundo :v
-      </Text>
-    </View>
-  )
-}
+    <SafeAreaView style={styles.fondo}>
+      <StatusBar backgroundColor="black" barStyle="light-content" />
+      <CalculadoraScreen />
+    </SafeAreaView>
+  );
+};
 
-export default App
+export default App;
