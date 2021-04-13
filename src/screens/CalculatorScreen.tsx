1e6ccc5c3f6d8561cd784be70cee0b5854adfcca
Juan Galarce <j6alarce@gmail.com>
2021-04-13 11:27:43 -0400

    Textos y primer button

---
+++ src/screens/CalculatorScreen.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+import {styles} from '../theme/appTheme';
+
+export const CalculatorScreen = () => {
+  return (
+    <View style={styles.calculatorContainer}>
+      <Text style={styles.resultSmall}>1,500.00</Text>
+      <Text style={styles.result}>1,500.00</Text>
+
+      <View>
+
+        <View style={styles.button}>
+          <Text style={styles.buttonText}>1</Text>
+        </View>
+
+      </View>
+    </View>
+  );
+};
--- src/theme/appTheme.tsx
+++ src/theme/appTheme.tsx
@@ -1,12 +1,37 @@
 import {StyleSheet} from 'react-native';
 
 export const styles = StyleSheet.create({
-  fondo: {
+  background: {
     flex: 1,
     backgroundColor: 'black',
   },
-  texto: {
+  calculatorContainer: {
+    flex: 1,
+    paddingHorizontal: 20,
+    justifyContent: 'flex-end',
+  },
+  result: {
     color: 'white',
-    fontSize: 20
-  }
+    fontSize: 60,
+    textAlign: 'right',
+  },
+  resultSmall: {
+    color: 'rgba(255,255,255,0.3)',
+    fontSize: 30,
+    textAlign: 'right',
+  },
+  button: {
+    height: 80,
+    width: 80,
+    backgroundColor: '#9B9B9B',
+    borderRadius: 100,
+    justifyContent: 'center',
+  },
+  buttonText: {
+    textAlign: 'center',
+    padding: 10,
+    fontSize: 30,
+    color: 'black',
+    fontWeight: 'bold',
+  },
 });
