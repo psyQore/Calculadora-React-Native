14a5f4ac6c77c50ffe4380bb1f24bafd9b6b67ed
Juan Galarce <j6alarce@gmail.com>
2021-04-13 21:06:11 -0400

    Se terminan todos los botones y el style respectivo para cada uno, se agrega touchableOpacity

--- src/components/ButtonCal.tsx
+++ src/components/ButtonCal.tsx
@@ -1,24 +1,34 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, TouchableOpacity} from 'react-native';
 import {styles} from '../theme/appTheme';
 
 interface Props {
   text: string;
   color?: string;
+  ancho?: boolean;
 }
 
-const ButtonCal = ({text, color = '#2D2D2D'}: Props) => {
+const ButtonCal = ({text, color = '#2D2D2D', ancho = false}: Props) => {
   // Css
   const {button, buttonText} = styles;
 
   return (
-    <View
-      style={{
-        ...button,
-        backgroundColor: color,
-      }}>
-      <Text style={buttonText}>{text}</Text>
-    </View>
+    <TouchableOpacity>
+      <View
+        style={{
+          ...button,
+          backgroundColor: color,
+          width: ancho ? 180 : 80,
+        }}>
+        <Text
+          style={{
+            ...buttonText,
+            color: color === '#9B9B9B' ? 'black' : 'white',
+          }}>
+          {text}
+        </Text>
+      </View>
+    </TouchableOpacity>
   );
 };
 
--- src/screens/CalculatorScreen.tsx
+++ src/screens/CalculatorScreen.tsx
@@ -10,15 +10,41 @@ export const CalculatorScreen = () => {
       <Text style={styles.result}>1,500.00</Text>
 
       <View style={styles.row}>
-        
         <ButtonCal text="C" color="#9B9B9B" />
         <ButtonCal text="+/-" color="#9B9B9B" />
         <ButtonCal text="del" color="#9B9B9B" />
         <ButtonCal text="/" color="#FF9427" />
-
-        {/* #2D2D2D Gris Oscuro */}
-        {/* #FF9427 Naranjo */}
+      </View>
+      <View style={styles.row}>
+        <ButtonCal text="7" />
+        <ButtonCal text="8" />
+        <ButtonCal text="9" />
+        <ButtonCal text="X" color="#FF9427" />
+      </View>
+      <View style={styles.row}>
+        <ButtonCal text="4" />
+        <ButtonCal text="5" />
+        <ButtonCal text="6" />
+        <ButtonCal text="-" color="#FF9427" />
+      </View>
+      <View style={styles.row}>
+        <ButtonCal text="1" />
+        <ButtonCal text="2" />
+        <ButtonCal text="3" />
+        <ButtonCal text="+" color="#FF9427" />
+      </View>
+      <View style={styles.row}>
+        <ButtonCal text="0" ancho />
+        <ButtonCal text="." />
+        <ButtonCal text="=" color="#FF9427" />
       </View>
     </View>
   );
 };
+
+{
+  /* #2D2D2D Gris Oscuro */
+}
+{
+  /* #FF9427 Naranjo */
+}
